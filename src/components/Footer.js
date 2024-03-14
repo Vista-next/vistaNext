bd82177bf387fcfa82ea96f7bb36faa0f33a645c
Megha Parmar <megha.d.parmar2018@gmail.com>
2024-03-14 22:22:44 +0530

    feat:worked on footer page ui

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,51 +1,62 @@
-// color: #999;
-//     font-size: 12px;
-//     font-weight: 500;
+import React from 'react';
 
-import EmailIcon from '@mui/icons-material/Email';
-import {Link} from 'react-router-dom'
-const Footer = () =>{
-    return <footer className = "bg-[#0b0c22] ">
-      <div className="">
-        
-          <div className="parthner flex flex-col py-4 px-[10%] space-y-4 sm:justify-center sm:items-center">
-            {/* <h6 className="text-white font-semibold">Partnered with Innovative Globally</h6>
-            <ul className="flex flex-col  md:flex-row ">
-                <li><a href="#."><img src="https://demos.webicode.com/html/ico-chain/html/images/c-mg-1.png" alt=""/></a></li>
-                <li><a href="#."><img src="https://demos.webicode.com/html/ico-chain/html/images/c-mg-2.png" alt=""/></a></li>
-                <li><a href="#."><img src="https://demos.webicode.com/html/ico-chain/html/images/c-mg-1.png" alt=""/></a></li>
-                <li><a href="#."><img src="https://demos.webicode.com/html/ico-chain/html/images/c-mg-2.png" alt=""/></a></li>
-                <li><a href="#."><img src="https://demos.webicode.com/html/ico-chain/html/images/c-mg-1.png" alt=""/></a></li>
-                <li><a href="#."><img src="https://demos.webicode.com/html/ico-chain/html/images/c-mg-2.png" alt=""/></a></li>
-                <li><a href="#."><img src="https://demos.webicode.com/html/ico-chain/html/images/c-mg-1.png" alt=""/></a></li>
-                <li><a href="#."><img src="https://demos.webicode.com/html/ico-chain/html/images/c-mg-2.png" alt=""/></a></li>
-        
-            </ul> */}
-        
-        </div>
-        
-  
-  
- {/* right */}
-
-      <div className="flex-col border-gray-500 border-t border-b-0 border-x-0 flex md:flex-row text-[#999] text-sm items-center w-full py-4 px-2">
-       
-        <div className=" col-md-6 w-[50%] flex flex-col sm:flex-row justify-center items-center py-2 px-2">
-          <Link to='/terms' className='sm:whitespace-nowrap sm:px-2 font-semibold '>Terms of Use </Link>
-          <p className="sm:whitespace-nowrap">Copyright © 2022 | MetaWebInu. |  All Rights Reserved. | MetaWebInu.com | Gmail: help@metawebinu.com</p>
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white py-8">
+      <div className="container mx-auto flex justify-between p-6 gap-1 flex-nowrap sm:flex-wrap md:flex-wrap lg:flex-nowrap">
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+          <h3 className="text-lg font-bold mb-4">About Us</h3>
+          <p>We're a cutting-edge software development company shaping future successes with innovative solutions</p>
         </div>
 
-          <div className="flex-col sm:flex-row space-y-2 sm:space-y-0 text-right  w-[50%] flex justify-center items-center space-x-2 py-2 px-2"> 
-          {/* <a href="#.">Faqs </a>  */}
-          {/* <a href="#." className="flex-nowrap">Terms &amp; Conditions </a> */}
-           <a href="mailto:help@metawebinu.com" className="whitespace-nowrap" > <EmailIcon/> Contact Us</a> </div>
-      
+        <div className="w-full sm:w-1/2 mt-2 md:w-1/3 lg:w-1/4">
+          <h3 className="text-lg font-bold mb-4">Services</h3>
+          <ul>
+            <li><a href="#">Mobile App Development</a></li>
+            <li><a href="#">Web Development</a></li>
+            <li><a href="#">Web Design</a></li>
+            <li><a href="#">Dedicated Developer</a></li>
+          </ul>
+        </div>
+        <div className="w-full sm:w-1/2 mt-2 md:w-1/3 lg:w-1/4">
+          <h3 className="text-lg font-bold mb-3">Office</h3>
+          <div className="flex flex-col mb-4">
+            <div className="flex items-center mb-2">
+              <i className="fa fa-map-marker text-base text-white mr-4"></i>
+              <div>
+                <h5 className="text-base text-white font-bold">Vista Next Solution</h5>
+                <span className="text-sm text-gray-400">shop no-5, 2nd floor satyam complex, Bopal Rd, near Samarpan Bunglows, Ahmedabad, Gujarat 380058</span>
+              </div>
+            </div>
+            <div className="flex items-center mb-2">
+              <i className="fa fa-phone text-base text-white mr-4"></i>
+              <h5 className="text-base text-white font-bold">+91258888887</h5>
+            </div>
+            <div className="flex items-center">
+              <i className="fa fa-envelope text-base text-white mr-4"></i>
+              <span className="text-sm text-gray-400">admin@gmail.com</span>
+            </div>
+          </div>
+        </div>
+        <div className="w-full sm:w-1/2 mt-2 md:w-1/3 lg:w-1/4">
+          <h3 className="text-lg font-bold mb-3">Follow Us</h3>
+          <div className="flex mb-4 text-2xl">
+            <a href="#" className="text-white mr-4"><i className="fa fa-skype"></i></a>
+            <a href="#" className="text-white mr-4"><i className="fa fa-facebook"></i></a>
+            <a href="#" className="text-white mr-4"><i className="fa fa-twitter"></i></a>
+            <a href="#" className="text-white mr-4"><i className="fa fa-instagram"></i></a>
+            <a href="#" className="text-white mr-4"><i className="fa fa-linkedin"></i></a>
+            <a href="#" className="text-white mr-4"><i className="fa fa-google"></i></a>
+            <a href="#" className="text-white"><i className="fa fa-apple"></i></a>
+          </div>
+        </div>
+
       </div>
-   
-
-    </div>
-
-  </footer>
-}
+      <div className="text-center mt-4">
+        <p>&copy; 2024 Your Company. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
