19e9147b3087c62f3d9f0da8f68e7f4180623e50
Megha Parmar <megha.d.parmar2018@gmail.com>
2024-03-15 22:23:56 +0530

    feat:ui/ux changes in footer page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,25 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-8">
-      <div className="container mx-auto flex justify-between p-6 gap-1 flex-nowrap sm:flex-wrap md:flex-wrap lg:flex-nowrap">
-        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+      <div className="flex mb-4 gap-3 justify-center text-2xl sm:text-xl md:text-xl lg:text-4xl">
+        <a href="#" className="text-white mr-4"><i className="fa fa-skype"></i></a>
+        <a href="#" className="text-white mr-4"><i className="fa fa-facebook"></i></a>
+        <a target="_blank" href="https://x.com/vistanexts?t=Afsei-NXLvz0IlBe1ohaVw&s=09
+" className="text-white mr-4"><i className="fa fa-twitter"></i></a>
+        <a target="_blank" href="https://www.instagram.com/vistanexts?igsh=MXMwZ2dvOGNmaGN2bQ==
+" className="text-white mr-4 cursor-pointer"><i className="fa fa-instagram"></i></a>
+        <a target="_blank" href="https://www.linkedin.com/in/vista-next-solution-02223a2ba" className="text-white mr-4"><i className="fa fa-linkedin"></i></a>
+        <a target="_blank" href="https://t.me/+G8bmBQBonC05OTll" className="text-white mr-4"><i className="fa fa-telegram"></i></a>
+
+      </div>
+
+      <div class="flex flex-wrap p-5">
+        <div class="w-full md:w-1/3 p-2">
           <h3 className="text-lg font-bold mb-4">About Us</h3>
           <p>We're a cutting-edge software development company shaping future successes with innovative solutions</p>
-        </div>
 
-        <div className="w-full sm:w-1/2 mt-2 md:w-1/3 lg:w-1/4">
+        </div>
+        <div class="w-full md:w-1/3 p-2">
           <h3 className="text-lg font-bold mb-4">Services</h3>
           <ul>
             <li><a href="#">Mobile App Development</a></li>
@@ -18,42 +30,39 @@ const Footer = () => {
             <li><a href="#">Dedicated Developer</a></li>
           </ul>
         </div>
-        <div className="w-full sm:w-1/2 mt-2 md:w-1/3 lg:w-1/4">
+        <div class="w-full md:w-1/3 p-2">
           <h3 className="text-lg font-bold mb-3">Office</h3>
           <div className="flex flex-col mb-4">
             <div className="flex items-center mb-2">
-              <i className="fa fa-map-marker text-base text-white mr-4"></i>
+              <span className='text-2xl sm:text-xl md:text-xl lg:text-2xl'>
+                <i className="fa fa-map-marker text-white mr-4"></i>
+              </span>
               <div>
                 <h5 className="text-base text-white font-bold">Vista Next Solution</h5>
-                <span className="text-sm text-gray-400">shop no-5, 2nd floor satyam complex, Bopal Rd, near Samarpan Bunglows, Ahmedabad, Gujarat 380058</span>
+                <p>shop no-5, 2nd floor satyam complex,</p>
+                <p>Bopal Rd, near Samarpan Bunglows,</p>
+                <p> Ahmedabad, Gujarat 380058</p>
+
               </div>
             </div>
             <div className="flex items-center mb-2">
-              <i className="fa fa-phone text-base text-white mr-4"></i>
+              <span className='text-2xl sm:text-xl md:text-xl lg:text-2xl'>
+                <i className="fa fa-phone text-base text-white mr-4"></i>
+              </span>
               <h5 className="text-base text-white font-bold">+91258888887</h5>
             </div>
             <div className="flex items-center">
-              <i className="fa fa-envelope text-base text-white mr-4"></i>
-              <span className="text-sm text-gray-400">admin@gmail.com</span>
+              <span className='text-2xl sm:text-xl md:text-xl lg:text-2xl'>
+                <i className="fa fa-envelope text-base text-white mr-4"></i>
+              </span>
+              <span className="text-sm text-white-400">admin@gmail.com</span>
             </div>
           </div>
         </div>
-        <div className="w-full sm:w-1/2 mt-2 md:w-1/3 lg:w-1/4">
-          <h3 className="text-lg font-bold mb-3">Follow Us</h3>
-          <div className="flex mb-4 text-2xl">
-            <a href="#" className="text-white mr-4"><i className="fa fa-skype"></i></a>
-            <a href="#" className="text-white mr-4"><i className="fa fa-facebook"></i></a>
-            <a href="#" className="text-white mr-4"><i className="fa fa-twitter"></i></a>
-            <a href="#" className="text-white mr-4"><i className="fa fa-instagram"></i></a>
-            <a href="#" className="text-white mr-4"><i className="fa fa-linkedin"></i></a>
-            <a href="#" className="text-white mr-4"><i className="fa fa-google"></i></a>
-            <a href="#" className="text-white"><i className="fa fa-apple"></i></a>
-          </div>
-        </div>
-
       </div>
+
       <div className="text-center mt-4">
-        <p>&copy; 2024 Your Company. All rights reserved.</p>
+        <p>&copy; 2024 Vista Next Solution. All rights reserved.</p>
       </div>
     </footer>
   );
